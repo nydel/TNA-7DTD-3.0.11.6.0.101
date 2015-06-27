de660007610b55706538ad6587070eb13c0d69a4
nydel <github@meerkat.cc>
2015-06-26 17:11:38 -0700

    base functionality

--- src/config.lisp
+++ src/config.lisp
@@ -7,6 +7,6 @@
 
 (in-package :TNA-config)
 
-(defparameter *server-addr* "localhost")
-(defparameter *server-port* 8081)
-(defparameter *server-tag* "[TNA]] ")
+(defparameter *server-addr* "nydel-700-147c")
+(defparameter *server-port* 23081)
+(defparameter *server-tag* "[GV1|TNA3]] ")
